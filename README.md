ffc53fdb660893165ed1be3ae634dcc329b17f1e
CorySimon <corysimon@berkeley.edu>
2020-01-21 12:49:55 -0800

    add sbmof1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://pubs.acs.org/doi/full/10.1021/jacs.6b10455 xtal downloaded from here and the DMF solvent was deleted via cutting out lines of the .cif
+
+https://pubs.acs.org/doi/full/10.1021/cg300274n SBMOF-1 downloaded from here, cg300274n_si_003.cif (14.66 kb), corresponding to the activated structure. (there are two, one activated, one not).
---
+++ read_cif.jl
@@ -0,0 +1,38 @@
+using PorousMaterials
+using Printf
+@eval PorousMaterials PATH_TO_CRYSTALS = pwd()
+
+# for viz
+f=Framework("NiPyC2.cif")
+strip_numbers_from_atom_labels!(f)
+f = replicate(f, (3,3,3))
+write_xyz(f)
+
+# for bonds
+f=Framework("NiPyC2.cif")
+strip_numbers_from_atom_labels!(f)
+write_xyz(f)
+infer_bonds!(f, false)
+write_bond_information(f, "NiPyC2_bonds.vtk")
+
+# iterate thru grpahs
+for ed in edges(f.bonds)
+    @printf("bond between atom %d (%s) and atom %d (%s)\n",
+        ed.src,
+        f.atoms.species[ed.src],
+        ed.dst,
+        f.atoms.species[ed.dst])
+end
+
+# get ids of N atoms
+ids_N = [i for i = 1:f.atoms.n_atoms][f.atoms.species .== :N]
+# find carbon's attached to the Ns. these are the "ortho" carbons
+ids_C_aro_ortho = []
+for id_N in ids_N
+    # get the neighbors of these N's
+    ids_neighbors = neighbors(f.bonds, id_N)
+    for id in ids_neighbors
+        @assert f.atoms.species[id] == :C
+    end
+end
+
