b65820712bbd33db6852824339ac2dcf35a07cc6
Nickolas Gantzler <gantzlen@oregonstate.edu>
2020-05-20 23:34:17 -0700

    organized repo, fixed file output locations for most functions, edited submission script.

---
+++ mof_construction/README.md
@@ -0,0 +1,7 @@
+directories for the functionalized mofs are names as follows:
+"MOF_replication_QE-calculation"
+crystal structures that have not been replicated will not have a "replication" section in the file name.
+
+The MOF whose parameters are experimentally will say "experiment" in the QE section. "relax" revers to fixed volume structural relaxation and "vc-relax" is variable cell relaxation.
+
+Example: "NiPyC2_sc211_relax" contains `.cif`,`.xyz`, and `.vtk` files for functionalized NiPyC2 which has been replicated with repfactors (2,1,1), and will be used for fixed volume structural optimization. 
--- submission_scripts/submit_qe_jobs.sh
+++ submission_scripts/submit_qe_jobs.sh
@@ -12,12 +12,6 @@
 #SBATCH -o pw.NiPyC2_P1.relax.out
 #SBATCH -e pw.NiPyC2_P1.relax.err
 module load slurm
-sbatch -J pw_NiPyC2_sc211_relax -A simoncor -p mime5 --ntasks=4 --cpus-per-task=1 -o pw_NiPyC2_sc211.relax.out -e pw_NiPyC2_sc211.relax.err qe_sims_submit.sh 
-
-
-###
-# for QE only use MPI, just keep the "proc" and "OMP_NUM_THREAD" values set at 1, 
-# and if you adjust "--ntasks" just make sure you also adjust the "-np" in mpiexec 
-# to the same value (e.g. 4 in this case)
-###
+qe_output_loc = "../structural_relaxation"
+sbatch -J pw.NiPyC2_sc211.relax -A simoncor -p mime5 --ntasks=8 --cpus-per-task=1 -o $qe_output_loc/pw.NiPyC2_sc211.relax.out -e $qe_output_loc/pw.NiPyC2_sc211.relax.err qe_sims_submit.sh 
 
