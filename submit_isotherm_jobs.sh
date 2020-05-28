1732616203ac8ebeffa47f319fc2ace4aa5e27f5
Nickolas Gantzler <gantzlen@oregonstate.edu>
2020-05-28 12:43:14 -0700

    removed gcmc_sims files and re-ran sims with UFF for Ar, Kr, and Xe. I edited the isotherm submit script to place output and error files in specific directories.

---
+++ submit_isotherm_jobs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+## we need to load Slurm so we can submit our jobs with sbatch
+module load slurm
+
+# NiPyC2_relax_meta_functonalized_OH.cif
+# NiPyC2_experiment.cif NiPyC2_relax.cif NiPyC2_vc-relax.cif
+for xtal in $(cat ./AA_mofs_to_sim.txt)
+do
+    if [ ! -d ./simulated_isotherm_data/$xtal ]; then
+	mkdir ./simulated_isotherm_data/$xtal
+    fi
+    for gas in Xe Kr Ar
+    do 
+        for FField in UFF.csv # Dreiding.csv
+        do 
+            echo "submitting job for $xtal with $gas using $FField"
+            sbatch -J $xtal$gas$FField -A simoncor -p mime5 -n 16\
+            -o simulated_isotherm_data/$xtal/"$xtal$gas$FField.o"\
+            -e simulated_isotherm_data/$xtal/"$xtal$gas$FField.e"\
+             --export=xtal="$xtal",gas="$gas",FField="$FField" gcmc_submit.sh
+        done
+    done
+done
+
+# NOTE: sbatch does not launch tasks, it requests an allocation of resources and submits a batch script
+# optional arguements can be found at https://slurm.schedmd.com/sbatch.html
+
+## SBATCH -J job_name # name that will apprear with id upon query
+## SBATCH -A simoncor # sponsored acount = research group
+## SBATCH -p mime5 # name of queue or partition to run on
+## SBATCH -n 4 # (--ntasks) how many job steps run within the allocated resources   
+## SBATCH --cpus-per-task=1 # how many processors per task | default=1) 
+##     arguement is set in julia submission as the "-p" flag, so this can be left as default.
+## SBATCH -o filename.out # output file
+## SBATCH -e filename.err # error file
+## SBATCH --mail-type=BEGIN,END,FAIL # event for mailing user
+## SBATCH --mail-user=myONID@oregonstate.edu # who will receive the email for the specified events
