46575d8bee2879e43728c71f8dc44bf690b5fa2f
Nickolas Gantzler <gantzlen@oregonstate.edu>
2020-07-01 10:40:02 -0700

    getting error message when running QE.

--- structural_relaxation/qe_sims_submit.sh
+++ structural_relaxation/qe_sims_submit.sh
@@ -2,15 +2,8 @@
 ###
 # for QE only use MPI, just keep the "proc" and "OMP_NUM_THREAD" values set at 1, 
 # and if you adjust "--ntasks" just make sure you also adjust the "-np" in mpiexec 
-# to the same value (e.g. 4 in this case)
+# to the same value.
 ###
-#SBATCH -J pw_NiPyC2_P1_relax
-#SBATCH -A simoncor
-#SBATCH -p mime5
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=1
-#SBATCH -o pw.NiPyC2_P1.relax.out
-#SBATCH -e pw.NiPyC2_P1.relax.err
 
 module load intel/19
 module load qe
@@ -18,5 +11,5 @@ which mpiexec
 # which pw.x
 export OMP_NUM_THREADS=1 
 echo "starting QE" `date`
-mpiexec -mca btl tcp,self -mca orte_base_help_aggregate 0 -np 32 ~/qe-6.5/bin/pw.x -procs 1 -npool 2 -in QE_input_scripts/$pw_input_filename
+mpiexec -mca btl tcp,self -mca orte_base_help_aggregate 0 -np 16 ~/qe-6.5/bin/pw.x -procs 1 -npool 2 -in ./QE_input_scripts/$pw_input_filename
 echo "QE finished" `date`
--- structural_relaxation/submit_qe_jobs.sh
+++ structural_relaxation/submit_qe_jobs.sh
@@ -2,25 +2,21 @@
 ###
 # for QE only use MPI, just keep the "proc" and "OMP_NUM_THREAD" values set at 1, 
 # and if you adjust "--ntasks" just make sure you also adjust the "-np" in mpiexec 
-# to the same value (e.g. 4 in this case)
+# to the same value.
 ###
 
 module load slurm
 
 for pw_input_filename in $(cat ./AA_mofs_to_relax.txt)
 do
-    # echo "$pw_input_filename"  
     # create the name that will be used for output and job names
     split_filename=(${pw_input_filename//./ })
     pw_sim_name=${split_filename[1]}
 
     ## submit the simulation
-    sbatch -J $pw_sim_name -A simoncor -p mime5 --ntasks=32 --cpus-per-task=1 --time=72:00:00 \
+    sbatch -J $pw_sim_name -A simoncor -p mime5 --ntasks=16 --cpus-per-task=1 --time=72:00:00 \
            --mail-type=ALL --mail-user=gantzlen \
-           -o /nfs/hpc/share/gantzlen/"$pw_sim_name.out" \ 
-           -e /nfs/hpc/share/gantzlen/"$pw_sim_name.err" \ 
+           -o /nfs/hpc/share/gantzlen/"$pw_sim_name.out" \
+           -e /nfs/hpc/share/gantzlen/"$pw_sim_name.err" \
            --export=pw_input_filename="$pw_input_filename" qe_sims_submit.sh
-    
-    ## after the simlation finishes, we delete the large, unwanted data files
-    # rm -rf QE_relaxation_results/pbesol_relax/*.save
 done
