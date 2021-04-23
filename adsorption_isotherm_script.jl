b7a5989cf7b84534cf54eec15fc2a3dd6a69b430
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-04-23 12:32:47 -0700

    fixed symmetry loop in experimental m-NH2 cifs and cleaned up isotherm script to work better for kwargs.

--- adsorption_isotherm_script.jl
+++ adsorption_isotherm_script.jl
@@ -1,5 +1,7 @@
 using PorousMaterials
-## define path to data folder (absolute path on hpc)
+###
+#  Define paths
+###
 @eval PorousMaterials PATH_TO_DATA = joinpath("/nfs/stak/users/gantzlen/DTRA/data")
 @info PorousMaterials.PATH_TO_DATA
 
@@ -9,9 +11,9 @@ using PorousMaterials
 # @eval PorousMaterials PATH_TO_CRYSTALS = joinpath(PorousMaterials.PATH_TO_DATA, "crystals", "tmp_xtal")
 @info PorousMaterials.PATH_TO_CRYSTALS
 
-
-
-# read in name of crystal structure, adsorbate, and forcefield from command line arguments
+###
+#  Read command line arguments: name of - crystal structure, adsorbate, and forcefield
+###
 if length(ARGS) != 3
     error("pass the crystal structure name as a command line argument followed by the adsorbate then the forcefield,
  	such as: julia cof_isotherm_sim.jl COF-102.cif Xe UFF.csv")
@@ -21,44 +23,34 @@ adsorbate = ARGS[2]
 ffield = ARGS[3]
 println("running mol sim in ", crystal, " with ", adsorbate, " and ", ffield)
 
-# read in crystal structure
-xtal = Crystal(crystal)
-strip_numbers_from_atom_labels!(xtal)
-
-## define simulation parameters
-mol  = Molecule(adsorbate)
-ljff = LJForceField(ffield, mixing_rules="Lorentz-Berthelot")
-temp =  298.0 # K
-
+###
+#  Set Simulation Parameters and keyword arguments
+###
 pmin   = -3   # in log10, units: bar
 pmax   = 1.0  # value of max pressure (actual value), units: bar
 nsteps = 25   # number of pressure intervals to split range
-pressures = 10 .^ range(pmin, stop=log10(pmax), length=nsteps) # bar
 
-n_sample_cycles = 50000 
-n_burn_cycles   = 50000
+sim_params = Dict("xtal"        => Crystal(crystal; remove_duplicates=true),
+                  "molecule"    => Molecule(adsorbate),
+                  "temperature" => 298.0,
+                  "pressures"   => 10 .^ range(pmin, stop=pmax, length=nsteps),
+                  "ljff"        => LJForceField(ffield)
+                 )
 
-# for low pressure ranges we can get away with using the ideal gas
-# equation of state (default), for high pressures use eos=:PengRobinson.
-# equation_of_state = :PengRobinson
+strip_numbers_from_atom_labels!(sim_params["xtal"])
 
-## assign sim output to a variable
-adsorption_data = adsorption_isotherm(xtal, mol, temp, pressures, ljff, 
-                                      n_burn_cycles=n_burn_cycles, 
-                                      n_sample_cycles=n_sample_cycles,
-                                      calculate_density_grid=false)
+kwargs = Dict("n_burn_cycles"   => 50000, 
+              "n_sample_cycles" => 50000,
+              "calculate_density_grid" => false
+             )
 
-
-# henry_result = henry_coefficient(xtal, mol, temp, ljff; insertions_per_volume=500)
-
-##  run density grid calculations over the xtal unit cell
-#n_sample_cycles = 25000
-#n_burn_cycles = 25000
-
-#press = 0.01 # bar [0.01, 0.10, 1.1]
-#gcmc_results = μVT_sim(xtal, mol, temp, press, ljff, 
-#                 n_burn_cycles=n_burn_cycles,
-#                 n_sample_cycles=n_sample_cycles,
-#                 calculate_density_grid=true,
-#                 density_grid_dx=0.2,
-#                 density_grid_sim_box=false) 
+###
+#  Run simulation
+###
+results = adsorption_isotherm(sim_params["xtal"],
+                              sim_params["molecule"],
+                              sim_params["temperature"],
+                              sim_params["pressures"],
+                              sim_params["ljff"];
+                              kwargs...
+                             )
--- gcmc_submit.sh
+++ gcmc_submit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # print date and time
-date
-julia -p 32 adsorption_isotherm_script.jl $xtal $gas $ljff
+echo "BEGIN \n" `date`
+julia -p 16 adsorption_isotherm_script.jl $xtal $gas $ljff
+date "END \n" `date`
