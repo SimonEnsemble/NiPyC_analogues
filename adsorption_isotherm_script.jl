4d159d7b0c8e2bd35004553c1b28da079a90ad39
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-04-26 10:58:21 -0700

    organizing repo. set up script to run density grid calcs over 2x2x2 unitcell for plotting visualization convenience

--- adsorption_isotherm_script.jl
+++ adsorption_isotherm_script.jl
@@ -40,8 +40,39 @@ sim_params = Dict("xtal"        => Crystal(crystal, remove_duplicates=true),
 strip_numbers_from_atom_labels!(sim_params["xtal"])
 
 kwargs = Dict(:n_burn_cycles   => 50000, 
-              :n_sample_cycles => 50000
+              :n_sample_cycles => 50000,
+              :calculate_density_grid => true,
+              :density_grid_sim_box   => false
              )
+
+###
+#  density grid config
+###
+if kwargs[:calculate_density_grid]
+    @warn "density grid being calculated over a 2x2x2 cell for plotting convenience"
+
+    sim_params["pressures"] = [0.01, 0.1, 1.0]
+
+    # which xtals do you want to calculate the grid for
+    xtals_of_interest = ["NiPyC2_experiment.cif",
+                         "NiPyC2_relax_sc211_meta_functionalized_NH2_pbesol_relax.cif",
+                         "NiPyC2_relax_sc211_meta_functionalized_CH3_pbesol_relax.cif",
+                         "NiPyC2_relax_sc211_meta_functionalized_N-C-O_pbesol_relax.cif"]
+
+    # set specific rep factors for plotting purposes
+    rep_factors = [(2 ,2, 2), (1, 2, 2), (1, 2, 2), (1, 2, 2)]
+
+    # only define if we are calculating grid
+    reps = Dict(zip(xtals_of_interest, rep_factors))
+
+    # redefine input xtal
+    sim_params["xtal"] = replicate(sim_params["xtal"], reps[crystal])
+
+    # write xyz and vtk for plotting
+    file_path = joinpath(pwd(), "figures/grid_images")
+    write_xyz(sim_params["xtal"].atoms, file_path * split(crystal, ".")[1] * "_222grid")
+    write_vtk(sim_params["xtal"].box, file_path * split(crystal, ".")[1] * "_222grid")
+end
 
 ###
 #  Run simulation
