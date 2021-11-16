cfbe1a9aad35c7310a6e9505af9a23d78415d157
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-11-15 17:26:57 -0800

    using N2 molecule radius as surface area probe in Zeo++ calcs and increased number of samples.

--- zeo_calculations/submit_zeo_calculations.sh
+++ zeo_calculations/submit_zeo_calculations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "#### START #### " `date`
 
-probe_radius=0.31 # atomic radius of He probe in Angstroms
+probe_radius=1.55 # atomic radius of N2 probe in Angstroms
 num_samples=5000  # number of Monte Carlo integration samples
 crystals_loc=../data/crystals
 
@@ -12,7 +12,7 @@ for xtal in $(cat ./AA_mofs_to_sim.txt)
 do
     ~/zeo++-0.3/network -stripatomnames -ha -res $output_loc/$xtal.res \
                         -vol $probe_radius $probe_radius 10*$num_samples $output_loc/$xtal.vol \
-                        -sa $probe_radius $probe_radius $num_samples $output_loc/$xtal.sa \
+                        -sa $probe_radius $probe_radius 100*$num_samples $output_loc/$xtal.sa \
                         $crystals_loc/$xtal
 done
 
