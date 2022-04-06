aa450dea198eabf853927fdb72ac9b97fa19c222
Nickolas Gantzler <gantzlen@oregonstate.edu>
2022-04-06 16:57:09 -0700

    ran MD for Xe and Kr in experimental NH2 MOF.

--- lammps_diffusion_calcs/put_atom_in_mof.jl
+++ lammps_diffusion_calcs/put_atom_in_mof.jl
@@ -1,16 +1,14 @@
 using PorousMaterials
 
-xtal        = Crystal("NiPyC2_experiment.cif")
+xtal        = Crystal("Pn_Ni-PyC-NH2.cif") # "NiPyC2_experiment.cif"
 xenon       = Molecule("Xe") # Kr
-temperature = 298.0
-pressure    = 1.0
+temperature = 298.0 # unit: K
+pressure    = 1.0   # unit: bar
 ljff        = LJForceField("UFF")
-n_cpv       = 10
+n_cpv       = 10 # cycles per volume
 fraction_burn_cycles = 0.5
 
-# res, mol = μVT_sim(xtal, xenon, temperature, pressure, ljff, n_cycles_per_volume=n_cpv, write_adsorbate_snapshots=true)
 res, mol = μVT_sim(xtal, xenon, temperature, pressure, ljff, n_cycles_per_volume=n_cpv)
-
 
 # get adsorbates present in the original unit cell of the xtal
 frac_mol = Frac.(mol[1], xtal.box)
@@ -28,17 +26,3 @@ xtal_with_gas = Crystal(name, xtal.box, atoms, xtal.charges)
 # write cif
 write_cif(xtal_with_gas)
 
-
-
-
-
-# calculate the number of MC cycles
-# separate total number of cycles evenly into burn_cycles and sample_cycles
-#nb_cycles = max(5, ceil(Int, n_cpv * xtal.box.Ω))
-#@assert (0.0 < fraction_burn_cycles) && (fraction_burn_cycles < 1.0) 
-#n_burn_cycles   = ceil(Int, nb_cycles * fraction_burn_cycles)
-#n_sample_cycles = ceil(Int, nb_cycles * (1 - fraction_burn_cycles))
-#filename = μVT_output_filename(xtal, xenon, temperature, pressure, ljff, n_burn_cycles, n_sample_cycles)
-#xyz_file = split(filename, ".jld2")[1]
-#print(xyz_file)
-#write_xyz(mol, xtal.box)
